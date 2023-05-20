6a22a265a3f601c70d87606315ef533d66aaa461
Dinis Safin <zakermanik322@mail.ru>
2023-05-20 05:08:39 +0500

    Доделал 3 лабу по Вью ЖС

---
+++ src/components/PostList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="pizzas" v-if="pizzas.length > 0">
+        <transition-group name="pizzas-list">
+            <post-item
+            v-for="pizza in pizzas"
+            :pizza="pizza"
+            :key="pizza.id"
+            @newReview="newReview"
+        />
+        </transition-group>
+    </div>
+    <h2 v-else style="color:red;">
+        Пицц нет(
+    </h2>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem"
+    export default {
+        components: {
+            PostItem,
+        },
+        props: {
+            pizzas: {
+                type: Array,
+                required: true, 
+            }
+        },
+        methods: {
+            newReview(Rev, id) {
+                this.$emit("Rev", Rev, id)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .pizzas {
+        display: grid;
+        grid-template-columns: auto auto auto auto auto;
+        justify-content: space-between;
+    }
+    .pizzas-list-move {
+        transition: transform .5s;
+    }
+</style>
